6ceb2ae7717d059910e0f3d44a7ed1babd5b31c0
Nurmuhammad <nur.nurbekov.01@gmail.com>
2022-10-18 12:06:52 +0600

    s3 demo

---
+++ src/test/java/com/javatechie/s3/S3fileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3fileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
